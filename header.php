b9bc2b847f35f01a0c8fe50917decb68a6e0b812
Jason Britton <velograph@gmail.com>
2015-08-11 12:08:42 -0700

    Hey! Favicon!

--- header.php
+++ header.php
@@ -12,6 +12,7 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
 <link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/images/favicon.png" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
 <?php wp_head(); ?>
