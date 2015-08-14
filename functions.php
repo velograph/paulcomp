8f22c735f9fc6351b8278451d9f88bf58baf4154
Jason Britton <velograph@gmail.com>
2015-08-14 06:55:31 -0700

    Include script files

--- functions.php
+++ functions.php
@@ -88,7 +88,11 @@ add_action( 'widgets_init', 'paulcomp_widgets_init' );
 function paulcomp_scripts() {
 	wp_enqueue_style( 'paulcomp-style', get_stylesheet_directory_uri() . '/css/style.css', false, filemtime(get_stylesheet_directory() . '/css/style.css') );
 
-	wp_enqueue_script( 'paulcomp-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+	wp_enqueue_script( 'paulcomp-site-scripts', get_template_directory_uri() . '/js/site-scripts.js', array(), '20130115', true );
+
+	wp_enqueue_script( 'paulcomp-sticky', get_template_directory_uri() . '/js/jquery.sticky.js', array(), '20130115', true );
+
+	wp_enqueue_script( 'paulcomp-slick', get_template_directory_uri() . '/js/slick.min.js', array(), '20130115', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
