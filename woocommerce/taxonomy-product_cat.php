9349a4990a53a4bb4ffcda75afe49913f86dce7d
Jason Britton <velograph@gmail.com>
2015-08-26 21:46:32 -0700

    List the categories in order with products.

--- woocommerce/taxonomy-product_cat.php
+++ woocommerce/taxonomy-product_cat.php
@@ -124,22 +124,67 @@ get_header(); ?>
 
 		<?php else: ?>
 
-			<?php if ( have_posts() ) : ?>
+			<?php
+			$current_term = get_term_by( 'slug', get_query_var( 'term' ), get_query_var( 'taxonomy' ) );
+			$terms = get_terms( 'product_cat', array(
+				'hide_empty' => 1,
+				'orderby' => 'name',
+				'child_of' => $current_term->term_id,
+			) );
+			?>
 
-				<?php /* Start the Loop */ ?>
-				<?php while ( have_posts() ) : the_post(); ?>
+			<?php
+			// now run a query for each animal family
+			foreach( $terms as $term ) {
 
-					<?php
-						/* Include the Post-Format-specific template for the content.
-						 * If you want to override this in a child theme, then include a file
-						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-						 */
-						get_template_part( 'content', get_post_format() );
-					?>
+			    // Define the query
+			    $args = array(
+			        'post_type' => 'product',
+			        'product_cat' => $term->slug
+			    );
+			    $query = new WP_Query( $args ); ?>
 
-				<?php endwhile; ?>
+				<h2><?php echo $term->name; ?></h2>
 
-			<?php endif; ?>
+				<div class="portal-container page-content">
+
+			        <?php while ( $query->have_posts() ) : $query->the_post(); ?>
+
+			        <div class="portal" id="post-<?php the_ID(); ?>">
+						<a href="<?php the_permalink(); ?>">
+							<?php $mobile_page_banner = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'page-banner-mobile'); ?>
+							<?php $tablet_page_banner = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'page-banner-tablet'); ?>
+							<?php $desktop_page_banner = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'page-banner-desktop'); ?>
+							<?php $retina_page_banner = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'page-banner-retina'); ?>
+
+							<picture>
+								<!--[if IE 9]><video style="display: none"><![endif]-->
+								<source
+									srcset="<?php echo $mobile_page_banner[0]; ?>"
+									media="(max-width: 500px)" />
+								<source
+									srcset="<?php echo $tablet_page_banner[0]; ?>"
+									media="(max-width: 860px)" />
+								<source
+									srcset="<?php echo $desktop_page_banner[0]; ?>"
+									media="(max-width: 1180px)" />
+								<source
+									srcset="<?php echo $retina_page_banner[0]; ?>"
+									media="(min-width: 1181px)" />
+								<!--[if IE 9]></video><![endif]-->
+								<img srcset="<?php echo $desktop_page_banner[0]; ?>">
+							</picture>
+							<h4>
+								<?php the_title(); ?>
+							</h4>
+						</a>
+			        </div>
+
+					<?php endwhile; ?>
+
+			    </div>
+
+			    <?php wp_reset_postdata(); } ?>
 
 		<?php endif; ?>
 
