93cf8cefb63417d3f6b17552d81066b65cf20d02
Jason Britton <velograph@gmail.com>
2015-08-30 21:26:31 -0700

    404 Page

--- 404.php
+++ 404.php
@@ -1,56 +1,44 @@
 <?php
 /**
- * The template for displaying 404 pages (not found).
+ * Template Name: 404
  *
  * @package Paul Component Engineering
  */
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+	<div id="primary" class="four-oh-four">
 
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'Paul Component Engineering' ); ?></h1>
-				</header><!-- .page-header -->
+		<div class="message">
+			<h3>That page doesn't seem to exist. But that's ok!</h3>
+			<a href="/shop" class="button">Go To Shop</a>
+		</div>
 
-				<div class="page-content">
-					<p><?php _e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'Paul Component Engineering' ); ?></p>
+		<?php $mobile_page_banner = wp_get_attachment_image_src(get_post_thumbnail_id( '1406' ), 'page-banner-mobile'); ?>
+		<?php $tablet_page_banner = wp_get_attachment_image_src(get_post_thumbnail_id( '1406' ), 'page-banner-tablet'); ?>
+		<?php $desktop_page_banner = wp_get_attachment_image_src(get_post_thumbnail_id( '1406' ), 'page-banner-desktop'); ?>
+		<?php $retina_page_banner = wp_get_attachment_image_src(get_post_thumbnail_id( '1406' ), 'page-banner-retina'); ?>
 
-					<?php get_search_form(); ?>
+		<div class="background-image">
+			<picture>
+				<!--[if IE 9]><video style="display: none"><![endif]-->
+				<source
+					srcset="<?php echo $mobile_page_banner[0]; ?>"
+					media="(max-width: 500px)" />
+				<source
+					srcset="<?php echo $tablet_page_banner[0]; ?>"
+					media="(max-width: 860px)" />
+				<source
+					srcset="<?php echo $desktop_page_banner[0]; ?>"
+					media="(max-width: 1180px)" />
+				<source
+					srcset="<?php echo $retina_page_banner[0]; ?>"
+					media="(min-width: 1181px)" />
+				<!--[if IE 9]></video><![endif]-->
+				<img srcset="<?php echo $image[0]; ?>">
+			</picture>
+		</div>
 
-					<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
-
-					<?php if ( basis_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php _e( 'Most Used Categories', 'Paul Component Engineering' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
-					<?php endif; ?>
-
-					<?php
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( __( 'Try looking in the monthly archives. %1$s', 'Paul Component Engineering' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-					?>
-
-					<?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
-
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-
-		</main><!-- #main -->
 	</div><!-- #primary -->
 
 <?php get_footer(); ?>
