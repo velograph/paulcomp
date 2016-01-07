4528cb720f4e3d866af1057796b74616a22eb945
Jason Britton <velograph@gmail.com>
2016-01-07 13:47:31 -0800

    Unstick the header

--- js/site-scripts.js
+++ js/site-scripts.js
@@ -9,13 +9,14 @@ jQuery(document).ready(function(){
 		});
 	}
 
-	if (jQuery(window).width() > 860) {
-
-		// Desktop sticky header
-		jQuery(".desktop-header").sticky({
-			topSpacing: 0
-		});
-	}
+	// Sticky header if the logo isn't the circle
+	// if (jQuery(window).width() > 860) {
+	//
+	// 	// Desktop sticky header
+	// 	jQuery(".desktop-header").sticky({
+	// 		topSpacing: 0
+	// 	});
+	// }
 
 
 });
